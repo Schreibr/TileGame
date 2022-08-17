e824fa5dff6835e3403cf6bccb87ced33a6da783
Romain Schreiber <rom1tgt@gmail.com>
2022-08-17 23:05:12 +0200

    clean up

---
+++ TilesetMap/TilesetMap.tsx
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="TilesetMap" tilewidth="16" tileheight="16" tilecount="56320" columns="160">
+ <editorsettings>
+  <export target="../tilesets/TilesetMap.tres" format="Godot"/>
+ </editorsettings>
+ <properties>
+  <property name="projectRoot" value="C:/Users/Romain/Documents/GitHub/TileGame/"/>
+  <property name="relativePath" value="C:/Users/Romain/Documents/GitHub/TileGame/tilesets/TilesetMap.tres"/>
+ </properties>
+ <image source="TilesetMap.png" width="2560" height="5632"/>
+ <tile id="6436">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="1.04348" y="16.0684">
+    <polygon points="-0.956522,0.0434783 0.0286918,-5.88183 1.97974,-11.0464 4.10293,-14.805 11.1898,-15.8092 14.9771,-15.9814 14.9771,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6437">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0869565" y="0.108697" width="16" height="3.95652"/>
+  </objectgroup>
+ </tile>
+ <tile id="6438">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.5625" y="16.2314">
+    <polygon points="1.46467,-0.111413 -0.0286918,-5.88183 -1.97974,-11.0464 -4.10293,-14.805 -11.1898,-15.8092 -14.5912,-16.3564 -14.5858,-0.26087"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6596">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6598">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6599">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0747109" y="0.274455" width="12.8071" height="2.92935"/>
+  </objectgroup>
+ </tile>
+ <tile id="6600">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.20515" y="-0.0298924" width="12.8071" height="2.92935"/>
+  </objectgroup>
+ </tile>
+ <tile id="6756">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0.0869565">
+    <polygon points="-0.956522,-0.0434783 0.0286918,5.88183 1.97974,11.0464 4.10293,14.805 11.1898,15.8092 14.9771,15.9814 14.9771,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6757">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6758">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="15.0206" y="0.130435">
+    <polygon points="0.956522,-0.0434783 -0.0286918,5.88183 -1.97974,11.0464 -4.10293,14.805 -11.1898,15.8092 -14.9771,15.9814 -14.9771,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6916">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="1.04348" y="16.0249">
+    <polygon points="-0.956522,0.0434783 0.0286918,-5.88183 1.97974,-11.0464 4.10293,-14.805 11.1898,-15.8092 14.9771,-15.9814 14.9771,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6917">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="3.95652"/>
+  </objectgroup>
+ </tile>
+ <tile id="6918">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="14.9771" y="15.9379">
+    <polygon points="0.956522,0.0434783 -0.0286918,-5.88183 -1.97974,-11.0464 -4.10293,-14.805 -11.1898,-15.8092 -14.9771,-15.9814 -14.9771,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6919">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.0869565" y="0.0570652" width="12.8071" height="2.92935"/>
+  </objectgroup>
+ </tile>
+ <tile id="6920">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="3.20515" y="0.100542" width="12.8071" height="2.92935"/>
+  </objectgroup>
+ </tile>
+ <tile id="7076">
+  <objectgroup draworder="index" id="2">
+   <object id="5" x="1.43478" y="8.86957" width="0.0434783"/>
+   <object id="7" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="7078">
+  <objectgroup draworder="index" id="2">
+   <object id="4" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="7236">
+  <objectgroup draworder="index" id="2">
+   <object id="5" x="0.975522" y="0.0286918">
+    <polygon points="-0.956522,-0.0434783 0.0286918,5.88183 1.97974,11.0464 4.10293,14.805 11.1898,15.8092 14.9771,15.9814 14.9771,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7237">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="7238">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="15.0641" y="0.0869565">
+    <polygon points="0.956522,-0.0434783 -0.0286918,5.88183 -1.97974,11.0464 -4.10293,14.805 -11.1898,15.8092 -14.9771,15.9814 -14.9771,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8678">
+  <properties>
+   <property name="projectRoot" value="C:/Users/Romain/Documents/GitHub/TileGame"/>
+   <property name="relativePath" value="C:/Users/Romain/Documents/GitHub/TileGame/tilesets/TilesetMap.tres"/>
+  </properties>
+ </tile>
+</tileset>
